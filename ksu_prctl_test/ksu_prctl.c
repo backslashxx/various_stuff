73d3970b33eb598daccd9d420ce983eb7cebd582
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-08-31 15:32:32 +0800

    full ver

--- ksu_prctl_test/ksu_prctl.c
+++ ksu_prctl_test/ksu_prctl.c
@@ -5,16 +5,51 @@
 
 int main(int argc, char *argv[]) {
 
-	if (3 != argc) {
-		printf("Usage: %s option arg2\n", argv[0]);
+	if (6 != argc) {
+		printf("Usage: %s option arg2 arg3 arg4 arg5\n", argv[0]);
 		return 1;
 	}
 
+/* 
+
+add this to ksu's prctl interface, then call me like
+./prctl 0xDEADBEEF 0xAABBCCDD 10000 0 0
+
+change 10000 to uid you want to set
+grab uid from 
+grep "weishu" /data/system/packages.list
+
+
+	#define CMD_SET_MANAGER_UID 0xAABBCCDD
+	if (arg2 == CMD_SET_MANAGER_UID) {
+
+		uid_t new_uid;
+		
+		if (copy_from_user(&new_uid, arg3, sizeof(new_uid))) {
+			pr_err("copy new uid failed\n");
+			return 0;
+		}
+		
+		ksu_set_manager_uid(new_uid);
+		pr_info("new manager uid: %d\n", new_uid);
+
+		if (copy_to_user(result, &reply_ok, sizeof(reply_ok))) {
+			pr_err("prctl reply error, cmd: %lu\n", arg2);
+		}
+
+		return 0;
+	}
+
+
+
+*/
+
+
 	unsigned long option = strtoul(argv[1], NULL, 0);
 	unsigned long arg2 = strtoul(argv[2], NULL, 0);
-	unsigned long arg3 = 0;
-	unsigned long arg4 = 0;
-	unsigned long arg5 = 0;
+	unsigned long arg3 = strtoul(argv[3], NULL, 0);
+	unsigned long arg4 = strtoul(argv[4], NULL, 0);
+	unsigned long arg5 = strtoul(argv[5], NULL, 0);
 
 	printf("SYS_prctl(%lu, %lu, %p, %p, %p)\n", option, arg2, &arg3, &arg4, &arg5);
 	
