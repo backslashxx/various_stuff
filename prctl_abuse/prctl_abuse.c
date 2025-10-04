cde0cebde25c214cdf09f03c54e22b95aed897fd
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-10-04 22:34:51 +0800

    prctl_abuse: add plain samples

--- prctl_abuse/prctl_abuse.c
+++ prctl_abuse/prctl_abuse.c
@@ -8,7 +8,7 @@
 // zig cc -target aarch64-linux -Oz -s -static prctl_bench.c -Wl,--gc-sections -o bench
 
 #define N_ITERATIONS 25000
-#define N_FAKE 9
+#define N_FAKE 20
 
 // https://github.com/wtarreau/mhz/blob/master/mhz.c
 __attribute__((hot, always_inline))
@@ -28,12 +28,25 @@ static void prctl_call(unsigned long option) {
 	syscall(SYS_prctl, option, (unsigned long)&dummy,
 			(unsigned long)&dummy, (unsigned long)&dummy, (unsigned long)&dummy);
 	asm volatile("" ::: "memory"); // barrier();
+	asm volatile("nop");
 }
 
 int main() {
 	unsigned long long t_start, t_end;
 	unsigned long long total_fake_time = 0, total_kernelsu_option_time = 0;
 	unsigned long long fake_option = 0, kernelsu_option = 0;
+
+	unsigned long long total_nop_prctl_time = 0;
+	unsigned long long t_start_nop, t_end_nop;
+
+	int k = 0;
+	do {
+		t_start_nop = time_now_ns();
+		prctl_call((unsigned long)0xFFFFFFFF);
+		t_end_nop = time_now_ns();
+		total_nop_prctl_time = total_nop_prctl_time + (t_end_nop - t_start_nop);
+		k++;
+	} while (k < (N_ITERATIONS * (N_FAKE +1) ));
 
 	int i = 0;
 	do {
@@ -60,14 +73,32 @@ int main() {
 	// printf("total_fake_time: %llu ns, fake_option: %llu iter\n", total_fake_time, fake_option);
 	// printf("total_kernelsu_option_time: %llu ns, kernelsu_option: %llu iter\n", total_kernelsu_option_time, kernelsu_option);
 
+	long long avg_nop_prctl_ns = (long long)(total_nop_prctl_time / k);
 	long long avg_fake_ns = (long long)(total_fake_time / fake_option); // 0xDEADBEED + 0xDEADBEEE
 	long long avg_real_ns = (long long)(total_kernelsu_option_time / kernelsu_option);
 	long long overhead_ns = avg_real_ns - avg_fake_ns;
+	
+	double ratio = (double)avg_real_ns / avg_nop_prctl_ns;
+	double percent_overhead = (ratio - 1.0) * 100.0;
+	if (percent_overhead < 0) // absolute value
+		percent_overhead = percent_overhead * -1 ;
+	
 
 	printf("iterations: %d\n", N_ITERATIONS);
-	printf("average !0xDEADBEEF: %lld ns\n", avg_fake_ns);
+	printf("average 0xFFFFFFFF: %lld ns\n", avg_nop_prctl_ns);
+	printf("average ~0xDEADBEEF: %lld ns\n", avg_fake_ns);
 	printf("average 0xDEADBEEF: %lld ns\n", avg_real_ns);
-	printf("off by: %lld ns\n", overhead_ns);
+	printf("!0xDEADBEEF to 0xDEADBEEF: %lld ns\n", overhead_ns);
+	printf("0xDEADBEEF/0xFFFFFFFF: +%.2f%%\n", percent_overhead);
+
+	if (percent_overhead >= 2.0)
+		printf("confidence: high\n");
+	else if (percent_overhead >= 1.0)
+		printf("confidence: maybe\n");
+	else if (percent_overhead > 0.25)
+		printf("confidence: low\n");
+	else
+		printf("confidence: none\n");
 
 	return 0;
 }
