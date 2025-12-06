7068457a68715e04082da6a013d4dd4f0326c4a0
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-12-06 20:10:44 +0800

    small_start: fixup envp pullout

--- small_start/small_rt.h
+++ small_start/small_rt.h
@@ -69,7 +69,7 @@ void prep_main(long *sp)
 {
 	long argc = *sp;
 	char **argv = (char **)(sp + 1);
-	char **envp = (char **)(sp + 2);
+	char **envp = argv + argc + 1; // we need to offset it by the number of argc's!
 
 	long exit_code = c_main(argc, argv, envp);
 	__syscall(SYS_exit, exit_code, NONE, NONE, NONE, NONE, NONE);
--- small_start/small_start.c
+++ small_start/small_start.c
@@ -9,7 +9,7 @@ static int c_main(int argc, char **argv, char **envp)
 	int i = 0;
 	const char *newline = "\n";
 	
-loop_start:
+argv_loop_start:
 	char *arg = argv[i];
 	int len = 0;
 	while(arg[len])
@@ -21,7 +21,23 @@ loop_start:
 	i++;
 
 	if (argv[i])
-		goto loop_start;
+		goto argv_loop_start;
+
+	i = 0;
+
+envp_loop_start:
+	char *env = envp[i];
+	len = 0;
+	while(env[len])
+		len++;
+
+	__syscall(SYS_write, 1, (long)env, len, NONE, NONE, NONE);
+	__syscall(SYS_write, 1, (long)newline, 1, NONE, NONE, NONE);
+
+	i++;
+	
+	if (env[i])
+		goto envp_loop_start;
 
 	return 0;
 }
@@ -30,7 +46,7 @@ void prep_main(long *sp)
 {
 	long argc = *sp;
 	char **argv = (char **)(sp + 1);
-	char **envp = (char **)(sp + 2);
+	char **envp = argv + argc + 1; // we need to offset it by the number of argc's!
 
 	long exit_code = c_main(argc, argv, envp);
 	__syscall(SYS_exit, exit_code, NONE, NONE, NONE, NONE, NONE);
