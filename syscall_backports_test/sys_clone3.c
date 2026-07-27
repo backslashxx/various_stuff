a6bbfc0bd410aea328f9d58c3f80a3efddd46ba5
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-07-27 22:27:47 +0800

    sys_clone3: small_rt

--- syscall_backports_test/sys_clone3.c
+++ syscall_backports_test/sys_clone3.c
@@ -4,15 +4,35 @@
 #include <stdlib.h>
 #include <errno.h>
 
+
+#include "small_rt.h"
+
 #ifndef SYS_clone3
 #define SYS_clone3 435
 #endif
 
-int main(int argc, char *argv[])
+int c_main(int argc, char *argv[], char *envp[])
 {
-	int n = syscall(SYS_clone3, NULL, NULL);
+	char *str = "syscall exists!\nsyscall does NOT exist!\n";
 
-	printf("syscall ret: %d\n", n);
+	int n = __syscall(SYS_clone3, NULL, NULL, NONE, NONE, NONE, NONE);
+
+	if (n == -ENOSYS)
+		str = str + 16;
+
+	__syscall(SYS_write, 1, (long)str, 24, NONE, NONE, NONE);
 
 	return 0;
+}
+
+__attribute__((used))
+void prep_main(long *sp)
+{
+	long argc = *sp;
+	char **argv = (char **)(sp + 1);
+	char **envp = argv + argc + 1; // we need to offset it by the number of argc's!
+
+	long exit_code = c_main(argc, argv, envp);
+	__syscall(SYS_exit, exit_code, NONE, NONE, NONE, NONE, NONE);
+	__builtin_unreachable();
 }
